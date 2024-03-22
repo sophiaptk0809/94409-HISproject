89fee9bf5f5c3bc69d324489c2d425866844be46
郭泊彤 <sophiakuo@kakuhakus-air.mynetworksettings.com>
2024-03-22 11:34:31 -0400

    add get_data file and github setup

--- get_data.R
+++ get_data.R
@@ -23,8 +23,6 @@ library(survey)
 library(MEPS)
 
 
-
-
 # Read Home Health Data
 hh2021 = read_MEPS(year = 2021, type = "HH")
 hh2020 = read_MEPS(year = 2020, type = "HH")
@@ -46,7 +44,6 @@ fyc2019 = read_MEPS(year = 2019, type = "FYC")
 fyc2018 = read_MEPS(year = 2018, type = "FYC")
 fyc2017 = read_MEPS(year = 2017, type = "FYC")
 
-class(hip2021)
 
 # write.csv(hh2021,'/Users/sophiakuo/Documents/24S-HCIS/Project/data/hh2021.csv')
 # write.csv(hh2020,'/Users/sophiakuo/Documents/24S-HCIS/Project/data/hh2020.csv')
@@ -66,20 +63,21 @@ class(hip2021)
 # write.csv(fyc2018,'/Users/sophiakuo/Documents/24S-HCIS/Project/data/fyc2018.csv')
 # write.csv(fyc2017,'/Users/sophiakuo/Documents/24S-HCIS/Project/data/fyc2017.csv')
 
-write.csv(hh2021,'hh2021.csv')
-write.csv(hh2020,'hh2020.csv')
-write.csv(hh2019,'hh2019.csv')
-write.csv(hh2018,'hh2018.csv')
-write.csv(hh2017,'hh2017.csv')
+# write.csv(hh2021,'hh2021.csv')
+# write.csv(hh2020,'hh2020.csv')
+# write.csv(hh2019,'hh2019.csv')
+# write.csv(hh2018,'hh2018.csv')
+# write.csv(hh2017,'hh2017.csv')
 
-write.csv(ip2021,'ip2021.csv')
-write.csv(ip2020,'ip2020.csv')
-write.csv(ip2019,'ip2019.csv')
-write.csv(ip2018,'ip2018.csv')
-write.csv(ip2017,'ip2017.csv')
+# write.csv(ip2021,'ip2021.csv')
+# write.csv(ip2020,'ip2020.csv')
+# write.csv(ip2019,'ip2019.csv')
+# write.csv(ip2018,'ip2018.csv')
+# write.csv(ip2017,'ip2017.csv')
 
-write.csv(fyc2021,'fyc2021.csv')
-write.csv(fyc2020,'fyc2020.csv')
-write.csv(fyc2019,'fyc2019.csv')
-write.csv(fyc2018,'fyc2018.csv')
-write.csv(fyc2017,'fyc2017.csv')
+# write.csv(fyc2021,'fyc2021.csv')
+# write.csv(fyc2020,'fyc2020.csv')
+# write.csv(fyc2019,'fyc2019.csv')
+# write.csv(fyc2018,'fyc2018.csv')
+# write.csv(fyc2017,'fyc2017.csv')
+
